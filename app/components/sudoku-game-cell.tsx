1a644ee5198c5f132b90099c5f96b40fe16a89bb
Cássio dos Santos Sousa <cassiosantossousa@gmail.com>
2025-12-30 02:13:19 -0300

    chore: still getting SSR

--- app/components/sudoku-game-cell.tsx
+++ app/components/sudoku-game-cell.tsx
@@ -2,10 +2,17 @@ import { IGridCell } from "../sudoku/sudoku"
 
 export default function SudokuGameCell({ cell } : { cell: IGridCell }) {
   const hasInitialValue = Boolean(cell.initialValue);
-  const initialValueClasses = hasInitialValue ? 'text-blue-700' : '';
-  return (
-    <div className={`border-black border w-7 h-7 font-bold ${initialValueClasses}`}>
+  return hasInitialValue ? (
+    <div
+      className={`flex justify-center align-center border-black border w-7 h-7 font-bold text-blue-700`}
+    >
       { cell.getValue() }
     </div>
-  );
+  ) : (
+    <button
+      className={`flex justify-center align-center border-black border w-7 h-7 font-bold text-black`}
+    >
+      { cell.getValue() }
+    </button>
+  )
 }
